09e7f7fc17afcd2eebbb644d29cfb04403cb7400
Marat <dante1937@mail.ru>
2021-08-04 23:11:25 +0300

    added test database and table creation

--- experiments.py
+++ experiments.py
@@ -1,10 +1,7 @@
 from pass_keeper import PassKeeper
 
 def main():
-    p = str(input("Type db password: "))
     keeper = PassKeeper('db')
-    keeper.db_test(p)
-
 
 if __name__ == '__main__':
     main()
--- pass_keeper.py
+++ pass_keeper.py
@@ -1,4 +1,6 @@
 import psycopg2 as db
+from psycopg2 import sql
+from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
 
 class Note:
     def __init__(self, source='-', login='-', password='-'):
@@ -14,13 +16,19 @@ class Note:
         return self.source, self.login, self.password
 
 class PassKeeper():
-    def __init__(self, mode):
+    def selfs_init(self):
         self.modes = ('db', 'file')
+        self.db_name   = "passes_db"
+        self.file_name = "passes_file.csv"
+        self.__db_pass = None
 
+    def __init__(self, mode):
+        self.selfs_init()
         if mode not in self.modes:
             raise ValueError('Invalid value for ClassKeeper. Available values - db, file')
-
         self.mode = mode
+
+        if mode == 'db': self.create_db()
 
     def __save_in_db(self, notes):
         print("Saved in db")
@@ -35,26 +43,66 @@ class PassKeeper():
         if self.mode == 'file':
             self.__save_in_file(notes)
 
+    def create_db(self):
+        try:
+            p = str(input("Type db password: "))
+
+            con = db.connect(user = 'postgres', password = p, host = 'localhost', port = '5432')
+            con.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT) # <-- ADD THIS LINE
+            cursor = con.cursor()
+
+            cursor.execute("SELECT datname FROM pg_database;")
+            db_list = cursor.fetchall()
+
+            if (self.db_name,) not in db_list:
+                #Creating a database
+                sql = f'''CREATE DATABASE {self.db_name};'''
+                cursor.execute(sql)
+
+                #Creating table as per requirement
+                sql ='''CREATE TABLE notes(
+                        source CHAR(50) NOT NULL,
+                        login  CHAR(50) NOT NULL,
+                        password CHAR(50) NOT NULL
+                        );'''
+                cursor.execute(sql)
+
+                print("Database created successfully")
+            else:
+                print("DB is already exist")
+
+            con.close()
+
+            self.__db_pass = p
+        except db.OperationalError:
+            print("Error: Wrong password")
+
     def db_test(self, in_pass):
         try:
-            conn = db.connect(user = 'postgres', password = in_pass, host = 'localhost', port = '5432')
-
-            cursor = conn.cursor()
-
-            sql = '''CREATE database mydb;'''
+            con = db.connect(user = 'postgres', password = in_pass, host = 'localhost', port = '5432')
+            con.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT) # <-- ADD THIS LINE
+            cursor = con.cursor()
 
             #Creating a database
-            cursor.execute(sql)
-            print("Database created successfully........")
+            # sql = '''CREATE DATABASE mydb;'''
+            # cursor.execute(sql)
+            # print("Database created successfully........")
 
-            # list_database = cursor.fetchall()
-            # print(list_database)
+            cursor.execute("SELECT datname FROM pg_database;")
+            db_list = cursor.fetchall()
 
+            print(len(db_list))
+
+            for item in db_list:
+                if 'mydb' in item:
+                    print("It's here!")
+                    print(f"item = {item}, len = {len(item)}")
+            
             cursor.execute('select version()')
             data = cursor.fetchone()
             print(f'db version: {data}')
 
-            conn.close()
+            con.close()
 
             print('Connection closed')
         except db.OperationalError:
